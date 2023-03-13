cfa065bd69f297031ad0f548e98e2edfac7c1f87
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2023-03-13 12:02:17 -0500

    change port bind logic to use TcpListener type

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,14 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -17,9 +19,13 @@ async fn health_check_works() {
 }
 
 // Launch our application in the background
-fn spawn_app() {
-    let server = zero2new::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // Retrieve port assigned by OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2new::run(listener).expect("Failed to bind address");
     // tokio::spawn returns a handle to the spawned future
     // but no use for it
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
