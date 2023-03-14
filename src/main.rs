f99a9561a7fba1c742b3b6f139833ad547149428
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2023-03-13 22:08:30 -0500

    Add configuration file and associated logic

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::net::TcpListener;
+use zero2new::configuration::get_configuration;
 use zero2new::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port");
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration");
+    // Port is now coming from settings config
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
